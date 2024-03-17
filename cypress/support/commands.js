97341fd166de5b917060cd1239dcb1f38049d9a6
Ben Sihavong <sihavobe@oregonstate.edu>
2024-03-16 18:46:35 -0700

    second test for e2e done

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -25,3 +25,14 @@
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
 require("@testing-library/cypress/add-commands")
+
+Cypress.Commands.add("data", function() {
+    cy.findByRole("button", { name: "+"}).click()
+    cy.findByRole("button", { name: "+"}).click()
+    cy.findByTestId("data-1").type('1')
+    cy.findByTestId("data-2").type('3')
+    cy.findByTestId("data-3").type('2')
+    cy.findByTestId("data-4").type('7')
+    cy.findByTestId("data-5").type('3')
+    cy.findByTestId("data-6").type('15')
+})
