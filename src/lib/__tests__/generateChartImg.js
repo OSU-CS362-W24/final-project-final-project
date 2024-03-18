a236eb5d8c6fffecde868599b4fc6d669dd7340b
HaoZhu02 <zhuh2@oregonstate.edu>
2024-03-18 01:12:57 -0700

    Valid chart types with valid parameters return a valid URL

--- src/lib/__tests__/generateChartImg.js
+++ src/lib/__tests__/generateChartImg.js
@@ -9,7 +9,7 @@ const generateChartImg = require('../generateChartImg')
 describe('generateChartImg Function Unit Testing', () => {
 
     // Valid line chart data returns a valid URL
-    test('Valid line chart data returns a valid URL', async () => {
+    test('Valid line chart parameters returns a valid URL', async () => {
         // Arrange - set up all the paramters to be passed in
         const type = 'line'
         const data = [{ x: 1, y: 15 }, { x: 2, y: 15 }, { x: 5, y: 15 }, { x: 7, y: 15 }]
@@ -20,7 +20,25 @@ describe('generateChartImg Function Unit Testing', () => {
 
         // Act - generate the image AKA image URL
         const imgUrl = await generateChartImg(type, data, xLabel, yLabel, title, color)
-        console.log(imgUrl)
+        // console.log(imgUrl)
+
+        // Assert - Check if the generated URL is a blob URL
+        expect(imgUrl).toMatch(/^blob:/)
+
+    })
+
+    // Valid scatter chart data returns a valid URL
+    test('Valid scatter chart parameters returns a valid URL', async () => {
+        // Arrange - set up all the paramters to be passed in
+        const type = 'scatter'
+        const data = [{ x: 1, y: 15 }, { x: 2, y: 15 }, { x: 5, y: 15 }, { x: 7, y: 15 }]
+        const xLabel = 'X axis'
+        const yLabel = 'Y axis'
+        const title = 'Title of Chart'
+        const color = 'red'
+
+        // Act - generate the image AKA image URL
+        const imgUrl = await generateChartImg(type, data, xLabel, yLabel, title, color)
 
         // Assert - Check if the generated URL is a blob URL
         expect(imgUrl).toMatch(/^blob:/)
@@ -28,6 +46,23 @@ describe('generateChartImg Function Unit Testing', () => {
     })
 
 
-    
+    // Valid bar chart data returns a valid URL
+    test('Valid bar chart parameters returns a valid URL', async () => {
+        // Arrange - set up all the paramters to be passed in
+        const type = 'bar'
+        const data = [{ x: 1, y: 15 }, { x: 2, y: 15 }, { x: 5, y: 15 }, { x: 7, y: 15 }]
+        const xLabel = 'X axis'
+        const yLabel = 'Y axis'
+        const title = 'Title of Chart'
+        const color = 'red'
+
+        // Act - generate the image AKA image URL
+        const imgUrl = await generateChartImg(type, data, xLabel, yLabel, title, color)
+
+        // Assert - Check if the generated URL is a blob URL
+        expect(imgUrl).toMatch(/^blob:/)
+
+    })
+
 
 })
