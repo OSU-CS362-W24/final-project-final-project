373d1115c6b0e0d54e2cde0717f18f8b3a401844
HaoZhu02 <zhuh2@oregonstate.edu>
2024-03-18 03:12:28 -0700

    Tests for x and y Label as empty strings still return valid blob URL

--- src/lib/__tests__/generateChartImg.js
+++ src/lib/__tests__/generateChartImg.js
@@ -182,7 +182,46 @@ describe('generateChartImg Function Unit Testing', () => {
     })
 
 
-    
+    // xLabel as an empty string returns valid blob URL
+    test('xLabel as an empty string returns valid blob URL', async () => {
+        // Arrange - set up all the paramters to be passed in
+        const type = 'line'
+        const data = [{ x: 1, y: 15 }, { x: 2, y: 15 }, { x: 5, y: 15 }, { x: 7, y: 15 }]
+        const xLabel = ''
+        const yLabel = 'Y axis'
+        const title = 'Title of Chart'
+        const color = 'red'
+
+        // Act - generate the image AKA image URL
+        const imgUrl = await generateChartImg(type, data, xLabel, yLabel, title, color)
+       
+
+        // Assert - Check if the generated URL is a string and valid blob URL 
+        expect(typeof imgUrl).toBe('string')
+        expect(imgUrl).toMatch(/^blob:/)
+
+    })
+
+    // yLabel as an empty string returns valid blob URL
+    test('yLabel as an empty string returns valid blob URL', async () => {
+        // Arrange - set up all the paramters to be passed in
+        const type = 'line'
+        const data = [{ x: 1, y: 15 }, { x: 2, y: 15 }, { x: 5, y: 15 }, { x: 7, y: 15 }]
+        const xLabel = 'X axis'
+        const yLabel = ''
+        const title = 'Title of Chart'
+        const color = 'red'
+
+        // Act - generate the image AKA image URL
+        const imgUrl = await generateChartImg(type, data, xLabel, yLabel, title, color)
+       
+
+        // Assert - Check if the generated URL is a string and valid blob URL 
+        expect(typeof imgUrl).toBe('string')
+        expect(imgUrl).toMatch(/^blob:/)
+
+    })
+
 
 
 })
