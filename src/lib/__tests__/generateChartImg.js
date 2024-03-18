85ae6964960f531d37c85a5ad73d18832a2afcb2
HaoZhu02 <zhuh2@oregonstate.edu>
2024-03-18 03:06:17 -0700

    Added the optional parameters tests for Line and Scatter Charts

--- src/lib/__tests__/generateChartImg.js
+++ src/lib/__tests__/generateChartImg.js
@@ -68,8 +68,8 @@ describe('generateChartImg Function Unit Testing', () => {
     })
 
 
-    // Optional title parameter ommitted returns a valid URL
-    test('Optional title parameter ommitted returns a valid blob URL', async () => {
+    // Bar Chart - Optional title parameter ommitted returns a valid URL
+    test('Bar Chart optional title parameter ommitted returns a valid blob URL', async () => {
         // Arrange - set up all the paramters to be passed in
         const type = 'bar'
         const data = [{ x: 1, y: 15 }, { x: 2, y: 15 }, { x: 5, y: 15 }, { x: 7, y: 15 }]
@@ -87,8 +87,8 @@ describe('generateChartImg Function Unit Testing', () => {
     })
 
 
-    // Optional color parameter ommitted returns a valid URL
-    test('Optional color parameter ommitted returns a valid blob URL', async () => {
+    // Bar Chart - Optional color parameter ommitted returns a valid URL
+    test('Bar chart optional color parameter ommitted returns a valid blob URL', async () => {
         // Arrange - set up all the paramters to be passed in
         const type = 'bar'
         const data = [{ x: 1, y: 15 }, { x: 2, y: 15 }, { x: 5, y: 15 }, { x: 7, y: 15 }]
@@ -104,5 +104,85 @@ describe('generateChartImg Function Unit Testing', () => {
         expect(imgUrl).toMatch(/^blob:/)
 
     })
+
+
+    // Line Chart - Optional title parameter ommitted returns a valid URL
+    test('Line Chart optional title parameter ommitted returns a valid blob URL', async () => {
+        // Arrange - set up all the paramters to be passed in
+        const type = 'line'
+        const data = [{ x: 1, y: 15 }, { x: 2, y: 15 }, { x: 5, y: 15 }, { x: 7, y: 15 }]
+        const xLabel = 'X axis'
+        const yLabel = 'Y axis'
+        const color = 'red'
+
+        // Act - generate the image AKA image URL
+        const imgUrl = await generateChartImg(type, data, xLabel, yLabel, null, color)
+
+        // Assert - Check if the generated URL is a string and valid blob URL
+        expect(typeof imgUrl).toBe('string')
+        expect(imgUrl).toMatch(/^blob:/)
+
+    })
+
+
+    // Line Chart - Optional color parameter ommitted returns a valid URL
+    test('Line chart optional color parameter ommitted returns a valid blob URL', async () => {
+        // Arrange - set up all the paramters to be passed in
+        const type = 'line'
+        const data = [{ x: 1, y: 15 }, { x: 2, y: 15 }, { x: 5, y: 15 }, { x: 7, y: 15 }]
+        const xLabel = 'X axis'
+        const yLabel = 'Y axis'
+        const title = 'Title of Chart'
+
+        // Act - generate the image AKA image URL
+        const imgUrl = await generateChartImg(type, data, xLabel, yLabel, title, null)
+
+        // Assert - Check if the generated URL is a string and valid blob URL
+        expect(typeof imgUrl).toBe('string')
+        expect(imgUrl).toMatch(/^blob:/)
+
+    })
+    
+    
+    // Scatter Chart - Optional title parameter ommitted returns a valid URL
+    test('Scatter Chart optional title parameter ommitted returns a valid blob URL', async () => {
+        // Arrange - set up all the paramters to be passed in
+        const type = 'scatter'
+        const data = [{ x: 1, y: 15 }, { x: 2, y: 15 }, { x: 5, y: 15 }, { x: 7, y: 15 }]
+        const xLabel = 'X axis'
+        const yLabel = 'Y axis'
+        const color = 'red'
+
+        // Act - generate the image AKA image URL
+        const imgUrl = await generateChartImg(type, data, xLabel, yLabel, null, color)
+
+        // Assert - Check if the generated URL is a string and valid blob URL
+        expect(typeof imgUrl).toBe('string')
+        expect(imgUrl).toMatch(/^blob:/)
+
+    })
+
+
+    // Scatter Chart - Optional color parameter ommitted returns a valid URL
+    test('Scatter chart optional color parameter ommitted returns a valid blob URL', async () => {
+        // Arrange - set up all the paramters to be passed in
+        const type = 'scatter'
+        const data = [{ x: 1, y: 15 }, { x: 2, y: 15 }, { x: 5, y: 15 }, { x: 7, y: 15 }]
+        const xLabel = 'X axis'
+        const yLabel = 'Y axis'
+        const title = 'Title of Chart'
+
+        // Act - generate the image AKA image URL
+        const imgUrl = await generateChartImg(type, data, xLabel, yLabel, title, null)
+
+        // Assert - Check if the generated URL is a string and valid blob URL
+        expect(typeof imgUrl).toBe('string')
+        expect(imgUrl).toMatch(/^blob:/)
+
+    })
+
+
+    
+
 
 })
