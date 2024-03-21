e37fe6932433832a8493ee946608cc3e7bf633ed
maratmuz <maratmuzaffarov21@gmail.com>
2024-03-21 02:48:34 -0700

    Fixed older tests and began work on a new one

--- integration_tests/integration.test.js
+++ integration_tests/integration.test.js
@@ -8,9 +8,15 @@ require("@testing-library/jest-dom")
 const domTesting = require("@testing-library/dom")
 const userEvent = require("@testing-library/user-event").default
 const path = require('path')
+const generateChartImg = require("../src/lib/generateChartImg")
 
 beforeEach(function() {
+    document.documentElement.innerHTML = ''
     jest.resetModules()
+    jest.restoreAllMocks()
+    jest.clearAllMocks()
+    document.getElementsByTagName('html')[0].innerHTML = ''
+    window.localStorage.clear()
 })
 
 async function initDomFromFiles(htmlPath, jsPath) {
@@ -69,7 +75,7 @@ test("Chart Builder recognizes when values are added to a chart", async function
     expect(xyInputs[5]).toHaveValue(6)
 })
 
-test("Chart Builder given alert when missing data", async function() {
+test("Chart Builder gives alert when missing data", async function() {
     //Arrange:
     await initDomFromFiles(path.join(__dirname, '../src/line/line.html'), path.join(__dirname, '../src/line/line.js'))
     const user = userEvent.setup()
@@ -93,7 +99,7 @@ test("Chart Builder given alert when missing data", async function() {
     alertSpy.mockRestore();
 });
 
-test("Chart Builder given alert when missing labels", async function() {
+test("Chart Builder gives alert when missing labels", async function() {
     //Arrange:
     await initDomFromFiles(path.join(__dirname, '../src/line/line.html'), path.join(__dirname, '../src/line/line.js'))
     const user = userEvent.setup()
@@ -156,8 +162,8 @@ test("Chart Builder clears all fields correctly", async function () {
     await user.type(chartColor, "{arrowdown}") // Decrease R
     await user.type(chartColor, "{arrowdown}") // Decrease G
     await user.type(chartColor, "{arrowdown}") // Decrease B
-    await user.type(xLabelInput, "Time")
-    await user.type(yLabelInput, "Value")
+    await user.type(xLabelInput, "Cats")
+    await user.type(yLabelInput, "Dogs")
     await user.click(addValueButton)
     await user.click(addValueButton)
     await user.click(addValueButton)
@@ -190,4 +196,64 @@ test("Chart Builder clears all fields correctly", async function () {
     expect(yLabelInput).toHaveValue("")
     expect(xyInputs.length).toBe(2)
 })
+
+test("generateChartImg receives all user inputs correctly", async function () {
+    // Arrange:
+    await initDomFromFiles(path.join(__dirname, '../src/line/line.html'), path.join(__dirname, '../src/line/line.js'))
+    const user = userEvent.setup()
+    const generateChartImg = require(path.join(__dirname, '../src/lib/generateChartImg.js'));
+
+    /Users/maratmuzaffarov/Downloads/CS362/final_project/src/lib/generateChartImg.js
+
+    // Acquire:
+    const generateChartImgSpy = jest.spyOn(generateChartImg, 'generateChartImg');
+    const titleField = domTesting.getByLabelText(document, "Chart title")
+    const chartColor = domTesting.getByLabelText(document, "Chart color")
+    const xLabelInput = domTesting.getByLabelText(document, "X label")
+    const yLabelInput = domTesting.getByLabelText(document, "Y label")
+    const addValueButton = domTesting.getByRole(document, "button", { name: "+" })
+    var xyInputs = domTesting.getAllByRole(document, "spinbutton")
+    const generateButton = domTesting.getByRole(document, "button", { name: /Generate chart/i })
+
+    // Act:
+    await user.type(titleField, "Cats vs Dogs")
+    await user.click(chartColor)
+    await user.type(chartColor, "{arrowdown}") // Decrease R
+    await user.type(chartColor, "{arrowdown}") // Decrease G
+    await user.type(chartColor, "{arrowdown}") // Decrease B
+    await user.type(xLabelInput, "Cats")
+    await user.type(yLabelInput, "Dogs")
+    await user.click(addValueButton)
+    await user.click(addValueButton)
+    await user.click(addValueButton)
+    await user.click(addValueButton)
+    await user.click(addValueButton)
+
+    //Acquire:
+    xyInputs = domTesting.getAllByRole(document, "spinbutton")
+
+    //Act:
+    await user.type(xyInputs[0], "1")
+    await user.type(xyInputs[1], "2")
+    await user.type(xyInputs[2], "3")
+    await user.type(xyInputs[3], "4")
+    await user.type(xyInputs[4], "5")
+    await user.type(xyInputs[5], "6")
+    await user.type(xyInputs[6], "7")
+    await user.type(xyInputs[7], "8")
+    await user.type(xyInputs[8], "9")
+    await user.type(xyInputs[9], "10")
+    await user.click(generateButton)
+
+    // Assert: Check if generateChartImg was called with the correct arguments
+    expect(generateChartImgSpy).toHaveBeenCalledTimes(1);
+    expect(generateChartImgSpy).toHaveBeenCalledWith(
+        "line",
+        [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+        "Cats",
+        "Dogs",
+        "Cats vs Dogs",
+        "#FC4503"
+    );
+});
 
